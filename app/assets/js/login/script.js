61d49c0341f5b0ba87f9249ca5cb0f16b0e469c5
jstolpe <stolpe.justin@gmail.com>
2024-12-17 20:15:58 -0600

    stream 5 updates

--- app/assets/js/login/script.js
+++ app/assets/js/login/script.js
@@ -1,3 +1,24 @@
 document.addEventListener( 'DOMContentLoaded', function() { // document ready
-	console.log( 'Log In Page Ready!' );
+	logInForm = new formHelper( { // create new form helper for login
+		formClassPrefix: 'login'
+	} );
+
+	// get log up button
+	var logInButtonElement = document.getElementsByClassName( 'button-login' )[0];
+
+	logInButtonElement.addEventListener( 'click', function( event ) { // on click for button
+		serverLogIn( logInForm );
+	} );
 } );
+
+/**
+ * Server log in
+ * 
+ * @param object logInForm
+ * @return void
+ */
+function serverLogIn( logInForm ) {
+	if ( logInForm.validate( 'frontend' ) ) { // front end passed validation
+		// send data to server
+	}
+}
